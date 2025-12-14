7b01c5a2bd34a45617ab752cdf0c2c0dd45557af
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2025-12-14 21:42:59 +0800

    Handle integers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -53,6 +53,9 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
@@ -70,6 +73,19 @@ func (l *Lexer) readIdentifier() string {
 	}
 
 	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
 
 func isLetter(ch byte) bool {
