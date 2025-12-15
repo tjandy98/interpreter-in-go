c107b242660c0e6dc5f7f92d1f99b36a1128e7f7
tjandy98 <3953059+tjandy98@users.noreply.github.com>
2025-12-15 17:55:20 +0800

    Add REPL

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/tjandy98/interpreter-in-go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
